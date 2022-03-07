5d558d6d10511c97c5df4eaa6cbca3a8b7aa9857
ahcock <srparkgogo@gmail.com>
2022-03-08 02:08:04 +0900

    install styled-components, operate initial setting for nextjs, prettier, create GlobalReset component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { GlobalStyleReset } from "../styles/globalStyleReset";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyleReset />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
