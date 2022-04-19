bea3168a23c1e5f4a5f129df967ab72e050e2a56
ahcock <srparkgogo@gmail.com>
2022-04-20 02:06:35 +0900

    connect mongodb atlas cluster

---
+++ lib/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+const option = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add Mongo URI to .env.local");
+}
+
+if (!!uri) {
+  if (process.env.NODE_ENV === "development") {
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, option);
+      global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+  } else {
+    client = new MongoClient(uri, option);
+    clientPromise = client.connect();
+  }
+}
+
+export default clientPromise;
