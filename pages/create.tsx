a3dc62027554458db80d69c04a5cff88c8156644
ahcock <srparkgogo@gmail.com>
2022-07-01 00:35:00 +0900

    (1) Make CustomGetStaticProps, CustomGetServersideProps types

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
-import { GetServerSideProps } from "next";
+import { CustomGetServerSideProps } from "./_app";
 
 const Create: FC = () => {
   return <div>크리에이트 페이지</div>;
 };
 
 //TODO: isPagePrivate props에 자동완성 시킬수는 없을까? type을 어디다 지정해두어야 될까?
-const getServerSideProps: GetServerSideProps = async () => {
+const getServerSideProps: CustomGetServerSideProps = async () => {
   return {
     props: {
       isPagePrivate: true,
